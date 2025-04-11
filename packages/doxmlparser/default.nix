0423f348ef97d7553b375ac7dac92eadfbf7e92b
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2025-04-11 09:51:00 +0200

    doxmlparser: 1.10.0 -> 1.13.2

--- packages/doxmlparser/default.nix
+++ packages/doxmlparser/default.nix
@@ -6,12 +6,12 @@
 
 buildPythonPackage rec {
   pname = "doxmlparser";
-  version = "1.10.0";
+  version = "1.13.2";
 
   src = fetchPypi {
     inherit pname version;
     format = "wheel";
-    sha256 = "sha256-XkzyVwVAWVoygoDAymV7UaCDUNPxTBqfWoQC9TQ3aDU=";
+    sha256 = "sha256-ZasSbn0/d7G7FfpohmY2FPaNBHO0iDHh+4IvUzJD62g=";
     python = "py3";
     dist = "py3";
   };
