40ef4e52e81d0359ba5ba14ae83060970ed549d9
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2025-04-11 09:55:11 +0200

    pyedbglib: 2.23.0.14 -> 2.24.2.18

--- packages/pyedbglib/default.nix
+++ packages/pyedbglib/default.nix
@@ -8,14 +8,14 @@
 buildPythonPackage rec {
 
   pname = "pyedbglib";
-  version = "2.23.0.14";
+  version = "2.24.2.18";
 
   src = fetchPypi {
     inherit pname version;
     format = "wheel";
     python = "py3";
     dist = "py3";
-    sha256 = "sha256-NOqJfS4Xk9lw5KWk99YVOcrW89slXYLFQUlTI8fiv+M=";
+    sha256 = "sha256-P+5Xm60Exx2oXeSi7Gj289S9i6ze1/s7ZxyIMe0zZsI=";
   };
 
   format = "wheel";
