c14ad2bdfbec02c31c33a49e7e79e2aa97545e5e
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2025-04-11 09:56:49 +0200

    pykitinfo: 0.4.0.6 -> 1.1.2.19

--- packages/pykitinfo/default.nix
+++ packages/pykitinfo/default.nix
@@ -12,14 +12,14 @@
 buildPythonPackage rec {
 
   pname = "pykitinfo";
-  version = "0.4.0.6";
+  version = "1.1.2.19";
 
   src = fetchPypi {
     inherit pname version;
     format = "wheel";
     python = "py3";
     dist = "py3";
-    sha256 = "sha256-XT9vVcIxr5dWEKXpOahIOX9BsUPqqmnNneQeH4DiUPw=";
+    sha256 = "sha256-5qAKjL+pztWNfRHC9Quxtt/DPvoyprhzkU5DEDOGAgA=";
   };
 
   format = "wheel";
