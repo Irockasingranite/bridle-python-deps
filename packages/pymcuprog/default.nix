9263182053a46219fc1ae03771e17136df07f597
Sarah Renkhoff <sarah.renkhoff@navimatix.de>
2025-06-24 09:05:11 +0200

    pymcuprog: 3.17.3.45 -> 3.19.4.61

--- packages/pymcuprog/default.nix
+++ packages/pymcuprog/default.nix
@@ -8,14 +8,14 @@
 buildPythonPackage rec {
 
   pname = "pymcuprog";
-  version = "3.17.3.45";
+  version = "3.19.4.61";
 
   src = fetchPypi {
     inherit pname version;
     format = "wheel";
     python = "py3";
     dist = "py3";
-    sha256 = "sha256-lOlWdYQ5frwwI37PhNpyTnz9N+87093AwhwdGLfZ7VY=";
+    sha256 = "sha256-Zr6HC27btBNEPn+lmIK21FBMO52KreyoXFXoNFnWRAg=";
   };
 
   format = "wheel";
